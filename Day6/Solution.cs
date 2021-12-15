b9b1a8fe9e5e2ed868caf4e8ba0b478aafc9c89f
Tomasz Raciborski <Tomasz.Raciborski@Point72.com>
2021-12-15 13:50:31 +0100

    day 6 part 2

--- Day6/Solution.cs
+++ Day6/Solution.cs
@@ -2,29 +2,42 @@
 {
     internal class Solution
     {
-        public int Solve()
+        public long Solve()
         {
-            var items = File.ReadAllLines("Day6/input")[0].Split(',').Select(x => int.Parse(x)).ToList();
-
-            for (int day = 0; day < 80; day++)
+            var items = File.ReadAllLines("Day6/input")[0].Split(',').Select(int.Parse).GroupBy(x => x).ToDictionary(x => x.Key, x => x.LongCount());
+            for (var i = 0; i < 9; i++)
             {
-                var len = items.Count;
-                for (int i = 0; i < len; i++)
+                if (!items.ContainsKey(i))
                 {
-                    var item = items[i];
-                    if (item == 0)
-                    {
-                        items[i] = 6;
-                        items.Add(8);
-                    }
-                    else
-                    {
-                        items[i]--;
-                    }
+                    items[i] = 0;
                 }
             }
 
-            return items.Count;
+            for (var day = 0; day < 256; day++)
+            {
+                var zeros = items[0];
+                for (var i = 0; i < 9; i++)
+                {
+                    if (i == 6)
+                        items[i] = items[i + 1] + zeros;
+                    else if (i == 8)
+                        items[i] = zeros;
+                    else
+                        items[i] = items[i + 1];
+                }
+            }
+
+            return Sum(items);
+        }
+
+        private long Sum(Dictionary<int, long> items)
+        {
+            var sum = 0L;
+            for (var i = 0; i < 9; i++)
+            {
+                sum += items[i];
+            }
+            return sum;
         }
     }
 }
