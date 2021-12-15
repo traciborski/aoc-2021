2741cbb5f75e80f01bef36373f895d7eaa1a301b
Tomasz Raciborski <Tomasz.Raciborski@Point72.com>
2021-12-15 12:36:38 +0100

    day 6 part 1

---
+++ Day6/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace aoc_2021.Day6
+{
+    internal class Solution
+    {
+        public int Solve()
+        {
+            var items = File.ReadAllLines("Day6/input")[0].Split(',').Select(x => int.Parse(x)).ToList();
+
+            for (int day = 0; day < 80; day++)
+            {
+                var len = items.Count;
+                for (int i = 0; i < len; i++)
+                {
+                    var item = items[i];
+                    if (item == 0)
+                    {
+                        items[i] = 6;
+                        items.Add(8);
+                    }
+                    else
+                    {
+                        items[i]--;
+                    }
+                }
+            }
+
+            return items.Count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿var result = new aoc_2021.Day5.Solution().Solve();
+﻿var result = new aoc_2021.Day6.Solution().Solve();
 Console.WriteLine(result);
