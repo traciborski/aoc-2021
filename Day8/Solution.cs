6f0f29d941a55b18f215b65237a73e513bfd8206
Tomasz Raciborski <tomasz.raciborski@softwareone.com>
2021-12-18 23:35:52 +0100

    day 8 part 1

--- Day8/Solution.cs
+++ Day8/Solution.cs
@@ -4,9 +4,34 @@
     {
         public long Solve()
         {
-            var positions = File.ReadAllLines("Day8/input")[0].Split(',').Select(int.Parse).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
-           
-            return -1;
+            var positions = File.ReadAllLines("Day8/input").Select(x => x.Split(" | "));
+
+            var digits = new[]
+            {
+                2,
+                3,
+                4,
+                7
+            };
+            var count = 0;
+            foreach (var position in positions)
+            {
+                var left = Normalize(position[0]);
+                var right = Normalize(position[1]);
+                foreach (var digit in digits)
+                {
+                    if (right.Count(x => x.Length == digit) >= 1 && left.Count(x => x.Length == digit) >= 1)
+                    {
+                        count += right.Count(x => x.Length == digit);
+                    }
+                }
+            }
+            return count;
+        }
+
+        private List<string> Normalize(string words)
+        {
+            return words.Split(" ").Select(x => new string(x.ToCharArray().OrderBy(x => x).ToArray())).ToList();
         }
     }
 }
